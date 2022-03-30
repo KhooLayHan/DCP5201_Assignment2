75ca5fa5f870ad6351c4a64a51d60a58aaa946bd
KhooLayHan <dummygithuber01@gmail.com>
2022-03-30 09:10:56 +0800

    Create IDGenerator.cpp

---
+++ IDGenerator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class IDGenerator // Singleton Design Pattern
+{
+public:
+    IDGenerator(const IDGenerator&) = delete;
+    void operator=(const IDGenerator&) = delete;
+
+    static IDGenerator& GetID()
+    {
+        static IDGenerator s_ID;
+        return s_ID;
+    }
+
+    static int GenerateID() { return GetID().GenerateIDImpl(); }
+
+    static int GetCurrentID() { return GetID().CurrentIDImpl(); }
+
+private:
+    int m_ID = 1; 
+    int GenerateIDImpl() { return m_ID++; }
+    int CurrentIDImpl() { return m_ID; }
+
+    IDGenerator() {};
+    ~IDGenerator() {};
+};
