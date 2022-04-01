003f5b815b18cd8d770561bef61b54cc8b9688e9
KhooLayHan <dummygithuber01@gmail.com>
2022-04-01 19:45:14 +0800

    Update User.cpp
    Now will ensure the Print() function from Database class is clean by appending extra padding.
    Not the solution I would preferred.

--- User.cpp
+++ User.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <string>
 
 class User
@@ -5,13 +6,27 @@ class User
 public:
     User() {}
 
-    User(const std::string& name)
+    User(const std::string& name) // Constructor to initialize the username
         : m_Name(name)
     {}
 
     ~User() {}
 
-    const std::string& GetName() const { return m_Name; }
+    const std::string& GetName() // Assuming the length or size of the username is not greater than max_size
+    {
+        static constexpr std::size_t max_size = 8;
+        
+        if (m_Name.size() >= max_size)
+            return m_Name;
+        
+        std::size_t padding_size = max_size - m_Name.size();
+
+        for (int i = 0; i != padding_size; i++)
+            m_Name.append(" ");
+
+        return m_Name;
+    }
+    
     void SetName(const std::string& name) { m_Name = name; }
 
     friend User operator<<(std::ostream& out, const User& user);
