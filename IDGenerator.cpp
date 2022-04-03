21c94e5c02a25caba89664ab0144623dfdbf8f80
KhooLayHan <dummygithuber01@gmail.com>
2022-04-03 08:33:29 +0800

    Update IDGenerator.cpp

--- IDGenerator.cpp
+++ IDGenerator.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 
 class IDGenerator // Singleton Design Pattern
 {
@@ -14,11 +15,25 @@ public:
 
     static int GenerateID() { return GetID().GenerateIDImpl(); }
     static int GetCurrentID() { return GetID().CurrentIDImpl(); }
+    static std::string GenerateIDToString() { return GetID().GenerateIDToStringImpl(); }
 
 private:
-    int m_ID = 1; 
+    int m_ID = 1;
+    std::string m_StringID; 
     int GenerateIDImpl() { return m_ID++; }
     int CurrentIDImpl() { return m_ID; }
+
+    std::string GenerateIDToStringImpl()
+    {
+        std::stringstream string_stream;
+        string_stream << GenerateIDImpl();
+        string_stream >> m_StringID;
+
+        if (m_StringID.size() == 1)
+            return m_StringID += " ";
+        else
+            return m_StringID;
+    }
 
     IDGenerator() {};
     ~IDGenerator() {};
