56b2c10da072657e7c6231bcfee9fefdac07bc4a
KhooLayHan <dummygithuber01@gmail.com>
2022-04-03 12:31:41 +0800

    Update Transportation.cpp

--- Transportation.cpp
+++ Transportation.cpp
@@ -5,7 +5,7 @@
 #include <sstream>
 #include <utility>
 
-class Transportation 
+class Transportation // Base class
 {
 public:
     Transportation() {}
@@ -18,7 +18,7 @@ public:
 
     // static std::string_view SetDefaultTransportTypeName() { return "None"; }
 
-    static std::string_view SetAirTransportTypeName(int index)
+    static std::string_view SetAirTransportTypeName(int index) // static AirTransport listing to prevent modification
     {
         static constexpr std::array names = {
             "Aeroplane", "Helicopter", "Jet", "Airship", "Hot Air Balloon", "Space Shuttle"
@@ -27,7 +27,7 @@ public:
         return names[index];
     }
 
-    static std::string_view SetLandTransportTypeName(int index)
+    static std::string_view SetLandTransportTypeName(int index) // static LandTransport listing to prevent modification
     {
         static constexpr std::array names = {
             "Car", "Truck", "Taxi", "Bus", "Train", "Ambulance", "Motorbike"
@@ -36,7 +36,7 @@ public:
         return names[index];
     }
 
-    static std::string_view SetWaterTransportTypeName(int index)
+    static std::string_view SetWaterTransportTypeName(int index) // static WaterTransport listing to prevent modification
     {
         static constexpr std::array names = {
             "Ship", "Sailboat", "Yacht", "Submarine"
@@ -49,18 +49,18 @@ public:
     {
         static constexpr std::size_t max_size = 16;
 
-        if (m_TransportationName == "")
-            m_TransportationName = "NONE";  
+        if (m_TransportationName == "") // Additional if-checker when transport does not actually exist
+            m_TransportationName = "NONE";  // If does not exists, set m_TransportationName to NONE
 
         if (m_TransportationName.size() >= max_size)
             return m_TransportationName;
         
-        std::size_t padding_size = max_size - m_TransportationName.size();
+        std::size_t padding_size = max_size - m_TransportationName.size(); // Getting the total padding size again 
 
-        if (padding_size == max_size) // Additional if-checker when transport does not actually exist
-            m_TransportationName = "                ";
+        if (padding_size == max_size) 
+            m_TransportationName = "                "; 
 
-        for (int i = 0; i != padding_size; i++)
+        for (int i = 0; i != padding_size; i++) // Appends at the end of the m_TransportationName string with the needed padding size
             m_TransportationName.append(" ");
 
         return m_TransportationName;
@@ -68,7 +68,7 @@ public:
 
     friend std::ostream& operator<<(std::ostream& out, const Transportation& transport)
     {
-        out << transport.m_TransportationName;
+        out << transport.m_TransportationName; // Using a friend function and operator overloading of << to get m_TransportationName from Base class. 
         return out;
     }
 
@@ -76,12 +76,12 @@ protected:
     std::string m_TransportationName;
 };
 
-class AirTransport : public Transportation
+class AirTransport : public Transportation // Derived class
 {
 public:
     AirTransport() {}
 
-    AirTransport(std::string&& name)
+    AirTransport(std::string&& name) // Using r-value references and move constructors to avoid copying
         : m_Name(std::move(name))
     {}
 
@@ -89,16 +89,16 @@ public:
 
     const std::string& GetTransportTypeName(int id)
     {
-        if (id >= s_AirTransportTypeSize)
-            return m_TransportationName = "ERROR";
+        if (id >= s_AirTransportTypeSize) // If-checker to check whether the id value is greater than 6 and assigns ERROR to m_TransportationName 
+            return m_TransportationName = "ERROR"; 
 
-        m_TransportationName = SetAirTransportTypeName(id);
+        m_TransportationName = SetAirTransportTypeName(id); // Else gets the name from listing and returns it
         return m_TransportationName;
     }
 
     friend std::ostream& operator<<(std::ostream& out, const AirTransport& air_transport)
     {;
-        out << static_cast<const Transportation&>(air_transport);
+        out << static_cast<const Transportation&>(air_transport); // Using static_cast of Derived class to Base class to get the m_TransportationName  
         return out;
     }
 
@@ -107,12 +107,12 @@ private:
     static constexpr int s_AirTransportTypeSize = 6;
 };
 
-class LandTransport : public Transportation
+class LandTransport : public Transportation // Derived class
 {
 public:
     LandTransport() {}
 
-    LandTransport(std::string&& name)
+    LandTransport(std::string&& name) // Using r-value references and move constructors to avoid copying
         : m_Name(std::move(name))
     {}
 
@@ -120,30 +120,30 @@ public:
 
     const std::string& GetTransportTypeName(int id)
     {
-        if (id >= s_LandTransportTypeSize)
+        if (id >= s_LandTransportTypeSize) // If-checker to check whether the id value is greater than 7 and assigns ERROR to m_TransportationName
             return m_TransportationName = "ERROR";
 
-       m_TransportationName = SetLandTransportTypeName(id);
+       m_TransportationName = SetLandTransportTypeName(id); // Else gets the name from listing and returns it
        return m_TransportationName;
     }
 
     friend std::ostream& operator<<(std::ostream& out, const LandTransport& land_transport)
     {
-        out << static_cast<const Transportation&>(land_transport);
+        out << static_cast<const Transportation&>(land_transport); // Using static_cast of Derived class to Base class to get the m_TransportationName  
         return out;
     }
 
 private:
     std::string m_Name;
-    static constexpr int s_LandTransportTypeSize = 7;
+    static constexpr int s_LandTransportTypeSize = 7; // Instead of using const which is done in run-time, constexpr ensures the variable is initialized in compile-time.
 };
 
-class WaterTransport : public Transportation
+class WaterTransport : public Transportation // Derived class
 {
 public:
     WaterTransport() {}
 
-    WaterTransport(std::string&& name)
+    WaterTransport(std::string&& name) // Using r-value references and move constructors to avoid copying
         : m_Name(std::move(name))
     {}
 
@@ -151,20 +151,20 @@ public:
 
     const std::string& GetTransportTypeName(int id)
     {   
-        if (id >= s_WaterTransportTypeSize)
-            return m_TransportationName = "ERROR";
+        if (id >= s_WaterTransportTypeSize) // If-checker to check whether the id value is greater than 4 and assigns ERROR to m_TransportationName
+            return m_TransportationName = "ERROR"; 
 
-        m_TransportationName = SetWaterTransportTypeName(id);
+        m_TransportationName = SetWaterTransportTypeName(id); // Else gets the name from listing and returns it
         return m_TransportationName;
     }
 
     friend std::ostream& operator<<(std::ostream& out, const WaterTransport& water_transport)
     {
-        out << static_cast<const Transportation&>(water_transport);
+        out << static_cast<const Transportation&>(water_transport); // Using static_cast of Derived class to Base class to get the m_TransportationName  
         return out;
     }
 
 private:
     std::string m_Name;
-    static constexpr int s_WaterTransportTypeSize = 4;
+    static constexpr int s_WaterTransportTypeSize = 4; 
 };
